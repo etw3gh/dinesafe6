b8ab7f4195cee5f82dd328cbd65c00288c8d4265
Eli Tabello <etabw3@gmail.com>
2017-03-22 17:40:09 -0400

    'tw'

--- src/components/twitter.js
+++ src/components/twitter.js
@@ -4,11 +4,11 @@ import { Timeline } from 'react-twitter-widgets'
 
 export class TwitterTL extends Component {
   render() {
+    const timelineHeight = window.innerHeight * 0.85;
     return (
       <Timeline
         dataSource={{sourceType: 'profile', screenName: 'mydinesafe' }}
-        options={{chrome: 'noscrollbar nofooter', username: 'Dinesafe', height: twitterHeight, width: '85%' }}
-        onLoad={() => {} } />
+        options={{chrome: 'noscrollbar nofooter', username: 'Dinesafe', height: timelineHeight, width: '85%' }} />
     )
   }
 }
