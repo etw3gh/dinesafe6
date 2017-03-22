c284e780886ef6ddafc2e5ef3d30220b1384b1d7
Eli Tabello <etabw3@gmail.com>
2017-03-22 17:24:52 -0400

    'end'

--- src/components/dinesafemain.js
+++ src/components/dinesafemain.js
@@ -52,7 +52,7 @@ class MainView extends Component {
     else if (V == views.OPENDATA) {
       return <InfoCard link='http://www1.toronto.ca/wps/portal/contentonly?vgnextoid=4a37e03bb8d1e310VgnVCM10000071d60f89RCRD'
                        icon='creative commons'
-                       icontxt='opendata.toronto.ca'
+                       icontxt='toronto.ca'
                        iconimg=''
                        img='../images/to.png'
                        meta='Toronto'
