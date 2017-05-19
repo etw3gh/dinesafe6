74d1e12e498600679fee51d10e8b4ab51e7a44fd
Eli Tabello <etabw3@gmail.com>
2017-05-19 12:17:42 -0400

    routes.js

---
+++ src/appConfig/routes.js
@@ -0,0 +1,17 @@
+/*
+An enumeration for routes. replaces views.js
+*/
+export const views = {
+  HOME: '/',
+  LIST: '/list',
+  MAP: '/map',
+  PHO: '/pho',
+  SEARCH: '/search',
+  OPENDATA: '/opendata',
+  LICENCE: '/licence',
+  SOURCE: '/source',
+  TWITTERBOT: '/twitterbot',
+  TWITTERHELP: '/twitterhelp',
+  HELP: '/help',
+  INFO: '/info'
+}
