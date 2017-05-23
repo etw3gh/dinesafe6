cfe478b05170ebaec822ceb9f1340676c799c8ae
Eli Tabello <etabw3@gmail.com>
2017-05-23 14:37:25 -0400

    semi colons purged from index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import 'core-js/fn/object/assign';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/main';
-ReactDOM.render(<App/>, document.getElementById('app'));
+import 'core-js/fn/object/assign'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/main'
+ReactDOM.render(<App/>, document.getElementById('app'))
