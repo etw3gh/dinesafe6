daac2cada79f8a5e6f9db5dc33b11640daa3c672
Eli Tabello <etabw3@gmail.com>
2017-06-08 17:34:16 -0400

    'added icon for mapview on home page'

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import { GithubCommits } from './githubcommits'
+import { Button, Icon } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+import { routes } from '../appConfig/routes'
 
 export class HomeIsTheSix extends Component {
   render() {
@@ -9,7 +12,7 @@ export class HomeIsTheSix extends Component {
         <p>
           This is a work in progress. Thanks for checking it out<br />
           All venues are in Toronto for now<br />
-          Main functionality is in the Map option (see sidebar)<br />
+        Main functionality is in the <Link to={routes.MAP}><Button primary content='Map view' icon='map' /></Link><br />
           You can check out the last 25 commits in the table below
         </p>
 
