72db101183bb5e7a79771f7b8d18d720026a8389
Eli Tabello <etabw3@gmail.com>
2017-06-09 13:08:53 -0400

    'home. messages.'

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { GithubCommits } from './githubcommits'
-import { Button } from 'semantic-ui-react'
+import { Button, Icon, Message } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 import { routes } from '../appConfig/routes'
 
@@ -8,17 +8,24 @@ export class HomeIsTheSix extends Component {
   render() {
     return (
       <div>
-        <h3>Welcome To dinesafe6</h3>
-        <p>
-          This is a work in progress. Thanks for checking it out<br />
-          All venues are in Toronto for now<br />
-          <br />
-          Geolocation currently touch and go on chrome for android. Firefox for android is more reliable.
-          <br />
-          Main functionality is in the <Link to={routes.MAP}><Button primary content='Map view' icon='map' /></Link><br />
-          You can check out the last 25 commits in the table below
-        </p>
-
+        <h3></h3>
+        <Message color='blue' header='Welcome To dinesafe6'
+                 list={['This is a work in progress','All venues are in Toronto for now']}>
+          <Message.Content>
+            <Link to={routes.MAP}><Button primary content='Map view' icon='map' /></Link>
+          </Message.Content>
+        </Message>
+        <br />
+        <Message error header='Issues'
+                 list={[
+                   'Geolocation on chrome for android not consistently working',
+                   'Server: may need to renew letsencrypt certificate for google maps api...',
+                   'Firefox for android is more reliable',
+                   'Modals not mobile friendly',
+                   'Backend: assumption that row_id would remain the same across xml releases was incorrect resulting in duplicate rows'
+                 ]} />
+        <br />
+        <h3>You can check out the last 25 commits in the table below</h3>
         <GithubCommits />
       </div>
     )
