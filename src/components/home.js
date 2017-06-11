b0143c99290d24213557d3a4d2fa38c54207b7ea
Eli Tabello <etabw3@gmail.com>
2017-06-11 14:25:44 -0400

    'pull req btn'

--- src/components/home.js
+++ src/components/home.js
@@ -41,14 +41,15 @@ export class HomeIsTheSix extends Component {
         <h3>You can check out the last 25 commits in the table below</h3>
         <h4>
           <a href={Urls.github.issues} target='_blank'>
-            <Button color='red' icon='exclamation circle' size='large' label={{basic:true, color: 'red', pointing: 'left', content: 'Raise An Issue'}} />
+            <Button color='red' icon='exclamation circle' label={{basic:true, color: 'red', pointing: 'left', content: 'Raise An Issue'}} />
           </a>
         </h4>
         <h4>
           <a href={Urls.github.pulls} target='_blank'>
-            <Button color='green' icon='graduation' size='large' label={{basic:true, color: 'green', pointing: 'left', content: 'Pull Request'}} />
+            <Button color='green' icon='graduation' label={{basic:true, color: 'green', pointing: 'left', content: 'Pull Request'}} />
           </a>
         </h4>
+        <br />
         <GithubCommits />
       </div>
     )
