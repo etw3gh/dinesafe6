8ed97f4fb733e2fe925214521247f2bfe177de4c
Eli Tabello <etabw3@gmail.com>
2017-06-15 12:27:09 -0400

    'add coreux link'

--- src/components/home.js
+++ src/components/home.js
@@ -16,32 +16,13 @@ export class HomeIsTheSix extends Component {
           <Message.Content>
             <Label color='blue'>
               <Icon size='huge' name='chain'/>
-                <Link style={{fontSize: '20px'}} to={routes.MAP}>Map </Link>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <Link style={{fontSize: '20px'}} to={routes.PHO}>Pho </Link>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <Link style={{fontSize: '20px'}} to={routes.MAP}>Main View </Link>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <a style={{fontSize: '20px'}} href='https://ds6.ca'>CoreUI Refactor </a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
              </Label>
           </Message.Content>
         </Message>
         <br />
-        <Message color='green' header='TODOs'
-                 list={[
-                   'Download or email inspection data for a venue (csv/pdf)',
-                   'Make map markers interactive',
-                   'Allow mobile users to turn on/off map',
-                   'Allow user to drop home marker if geoloc fails or is inaccurate',
-                   'User assisted yelp search. stash images and yelp_id in mongo',
-                   'User assisted twitter search. stash avatar images, handle and twitter_id in mongo',
-                   'Wire up mongodb to allow user image upload, and admin features',
-                   'Sortable & filterable columns',
-                   'Search View',
-                   'Status / Severity legend',
-                   'Show nearby venues when in the inspection view of a venue' ]} />
-        <br />
-        <Message error header='Issues'
-                 list={[
-                   'separate venue types in reducer: pho/regular/beer/coffee/other....',
-                   'isolate map from menu swipe events (mobile)',
-                   'nav css. icon colors for image icons',
-                   'Geolocation on chrome for android not consistently working' ]} />
+        <Message color='green' header='INFO'>Being completeley refactored using CoreUI <a href='https://ds6.ca'>(ds6.ca)</a></Message>
         <br />
         <h3>You can check out the last 25 commits in the table below</h3>
         <h4>
