b488900c2d95f9995046ef1bce485431dab9e8b2
Eli Tabello <etabw3@gmail.com>
2017-10-19 16:21:56 -0400

    'tidy clipjs'

--- src/components/clip.js
+++ src/components/clip.js
@@ -5,8 +5,13 @@ import CopyToClipboard from 'react-copy-to-clipboard';
 export default class Clip extends Component {
   render() {
     return (
-      <CopyToClipboard text={this.props.text}>
-        <Icon name='copy' title='copy to clipboard' color='blue' size='large' />
+      <CopyToClipboard
+        text={this.props.text}>
+        <Icon
+          name='copy'
+          title='copy to clipboard'
+          color='blue'
+          size='large' />
       </CopyToClipboard>
     )
   }
